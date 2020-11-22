ec36d9fecdbaff30804dcc7b9288a35a93d150a5
wangwentao <wangwentao@bilibili.com>
2020-11-22 10:45:57 +0800

    pool v1.2

--- pool/elastic_pool.go
+++ pool/elastic_pool.go
@@ -3,70 +3,74 @@ package pool
 import (
 	"context"
 	"fmt"
-	"time"
+	"runtime"
+	"sync/atomic"
 )
 
 type Job struct {
-	WorkerQueueSize   int
-	MaxQueueCount     int
-	CurrentQueueCount int
-	workers           []chan *Worker
-	ctx               context.Context
-	cancel            context.CancelFunc
+	MaxQueueSize     int32
+	CurrentQueueSize int32
+	workerCh         chan *Worker
+	ctx              context.Context
+	cancel           context.CancelFunc
+	workerRunCount   int32
 }
 type Worker struct {
 	job *Job
 }
 
-func NewJob(workerQueueSize int, maxQueueCount int) *Job {
+func NewJob(currentQueueSize int32, maxQueueSize int32) *Job {
 	job := &Job{}
-	job.WorkerQueueSize = workerQueueSize
-	job.MaxQueueCount = maxQueueCount
-	job.CurrentQueueCount = maxQueueCount
-	job.workers = make([]chan *Worker, maxQueueCount)
-	for i := 0; i < maxQueueCount; i++ {
-		job.workers[i] = make(chan *Worker, workerQueueSize)
-		for k:=0;k<job.WorkerQueueSize;k++{
-			job.workers[i]<-&Worker{job: job}
-		}
-	}
+	job.MaxQueueSize = maxQueueSize
 	job.ctx, job.cancel = context.WithCancel(context.Background())
-
+	job.CurrentQueueSize = currentQueueSize
+	workerCh := make(chan *Worker, job.MaxQueueSize)
+	for i := int32(0); i < job.MaxQueueSize; i++ {
+		workerCh <- &Worker{job: job}
+	}
+	job.workerCh = workerCh
 	return job
 }
 func (job *Job) Dispatch(task func(ctx context.Context) error) {
 	worker := job.TakeWorker()
+	atomic.AddInt32(&job.workerRunCount, 1)
 	worker.Do(job.ctx, task)
 }
 func (job *Job) AddWorker(worker *Worker) {
-	for _, workerCh := range job.workers {
-		if len(workerCh) < job.WorkerQueueSize {
-			workerCh <- worker
-			break
-		}
-	}
+	job.workerCh <- worker
 }
 func (job *Job) TakeWorker() *Worker {
 	for {
-		select {
-		case <-job.ctx.Done():
-			return nil
-		default:
-
+		cur := atomic.LoadInt32(&job.CurrentQueueSize)
+		curWorker := atomic.LoadInt32(&job.workerRunCount)
+		if curWorker > cur {
+			runtime.Gosched()
+			continue
 		}
-		for i := 0; i < job.CurrentQueueCount; i++ {
-			if len(job.workers[i]) > 0 {
-				worker := <-job.workers[i]
-				return worker
-			}
-		}
-		time.Sleep(100 * time.Millisecond)
+		worker := <-job.workerCh
+		return worker
 	}
 
 }
+func (job *Job) Scale(ratio int32) {
+	scaleSize := job.CurrentQueueSize * ratio
+	if scaleSize > job.MaxQueueSize {
+		scaleSize = job.MaxQueueSize
+	}
 
+	job.CurrentQueueSize = scaleSize
+}
+func (job *Job) DeScale(ratio int32) {
+	scaleSize := job.CurrentQueueSize / ratio
+	if scaleSize == 0 {
+		scaleSize = 1
+	}
+	job.CurrentQueueSize = scaleSize
+
+}
 func (w *Worker) Do(ctx context.Context, task func(ctx context.Context) error) {
 	go func() {
+		defer atomic.AddInt32(&w.job.workerRunCount, -1)
 		err := task(ctx)
 		if err != nil {
 			fmt.Printf("%v", err)
--- pool/elastic_pool_test.go
+++ pool/elastic_pool_test.go
@@ -2,18 +2,109 @@ package pool
 
 import (
 	"context"
+	"fmt"
+	"math/rand"
+	"runtime"
+	"sync"
+	"sync/atomic"
 	"testing"
 	"time"
 )
 
 func TestJob(t *testing.T) {
-	job := NewJob(10, 200)
-	for i := 0; i < 10000; i++ {
+	job := NewJob(10, 100000)
+	res := int32(0)
+	count := 1000000
+	start := time.Now()
+	for i := 0; i < count; i++ {
 		job.Dispatch(func(ctx context.Context) error {
-			//t.Log("task complete")
+			time.Sleep(time.Duration(rand.Int31n(100)) * time.Millisecond)
+			atomic.AddInt32(&res, 1)
+			if res%int32(100) == 0 {
+				job.Scale(2)
+			}
 			return nil
 		})
 	}
-	t.Log("task complete")
-	time.Sleep(time.Second * 10)
+	for {
+		if res == int32(count) {
+			fmt.Println(res, time.Since(start), job.CurrentQueueSize)
+			return
+		} else {
+			mem := runtime.MemStats{}
+			runtime.ReadMemStats(&mem)
+			fmt.Println(mem.TotalAlloc, runtime.NumGoroutine())
+			time.Sleep(time.Millisecond)
+		}
+	}
+
+}
+func TestJobForCpuCompute(t *testing.T) {
+	count := 100
+	res := uint64(0)
+	workerCount := int32(runtime.NumCPU())
+	job := NewJob(workerCount, workerCount)
+
+	wg := sync.WaitGroup{}
+	wg.Add(count)
+	computeTask := func(ctx context.Context) error {
+		defer wg.Done()
+		sum := uint64(0)
+		for i := 0; i < 1000000000; i++ {
+			sum += uint64(i)
+		}
+		atomic.AddUint64(&res, sum)
+		return nil
+	}
+
+	for i := 0; i < count; i++ {
+		job.Dispatch(computeTask)
+	}
+	wg.Wait()
+	t.Log(res)
+}
+func TestJobForIOCompute(t *testing.T) {
+	count := 10000
+	workerCount := int32(runtime.NumCPU()) * 100
+	job := NewJob(workerCount, workerCount)
+
+	wg := sync.WaitGroup{}
+	wg.Add(count)
+	computeTask := func(ctx context.Context) error {
+		defer wg.Done()
+
+		time.Sleep(time.Duration(rand.Int31n(100)) * time.Millisecond)
+		return nil
+	}
+
+	for i := 0; i < count; i++ {
+		job.Dispatch(computeTask)
+	}
+	wg.Wait()
+}
+func TestJobForIOComputeWithDeathLock(t *testing.T) {
+	count := runtime.NumCPU() + 1
+	workerCount := int32(runtime.NumCPU())
+	job := NewJob(workerCount, workerCount)
+
+	wg := sync.WaitGroup{}
+	wg.Add(count)
+	res := make(chan int)
+
+	computeTask := func(ctx context.Context) error {
+		defer wg.Done()
+		res <- 1
+		return nil
+	}
+
+	for i := 0; i < count; i++ {
+		job.Dispatch(computeTask)
+	}
+	go func() {
+		for i := 0; i < count; i++ {
+			<-res
+		}
+
+	}()
+	wg.Wait()
 }
